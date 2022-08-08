6a2e769fd8c6003f7ec09aa03f699ec5efe24f63
hrs <h.r.szpuk@outlook.com>
2022-08-09 00:28:26 +0100

    Added memstack_t and new functions
    This commit changed a lot, I added a memstack structure which acts as a wrapper for the memstack_chain_ptr_t linked list nodes. The memstack structure basically replaces the global "first" and "last" variables at the cost of needing a memstack structure as the argument for msalloc().
    I also added a lot of comments to make sure you can understand what I've changed. I've also embedded the typedef into the struct definition itself.
    
    Changelog:
    - Moved memstack_chain_ptr_t into include/memstack.h
    - Created memstack_t to store "first" and "last"
    - Added new functions that will be present in src/memstack.c in the next commit
    - Moved the original functions into a comment at the bottom of the file
    - Moved typedef statement embedded into the struct statement itself

--- include/memstack.h
+++ include/memstack.h
@@ -4,12 +4,31 @@
 
 // Header file
 
-
 #ifndef MC_MEMSTACK_H
 #define MC_MEMSTACK_H
 
-extern void msinit();
-extern void* msalloc(int size);
-extern void msfree();
+// Linked list node.
+// Stores a ptr to the memory allocated and the next element in the linked list.
+// If *next* is NULL then that node is the last node in the linked list.
+typedef struct memstack_chain_ptr_t{
+    void* ptr;  // Pointer to the memory allocated
+    struct memstack_chain_ptr_t* next;  // Pointer to next node
+} memstack_chain_ptr;
+
+// Memstack structure.
+// Stores the linked list for all of our allocations.
+typedef struct memstack_t {
+    memstack_chain_ptr* last;  // Stores last element
+    memstack_chain_ptr* first;  // Stores first element
+} memstack;
+
+extern memstack* new_memstack();  // Creates a new memstack
+extern void* msalloc(memstack* storage, int size);  // Allocate onto memstack and return pointer
+extern void msfree(memstack* storage);  // Free all of memstack
+
+// Legacy functions
+//extern void msinit();
+//extern void* msalloc(int size);
+//extern void msfree();
 
 #endif
